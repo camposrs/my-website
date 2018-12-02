9b276549790be72508e72e718cdbc51b5547ca41
sergcr1993 <37964219+sergcr1993@users.noreply.github.com>
2018-12-02 15:08:07 -0800

    Add files via upload

---
+++ homefunction.js
@@ -0,0 +1,10 @@
+//JS sourced from JQuery and Slick frameworks
+$(document).on('ready', function()
+{
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+});
